530a1e94d173fdeee319a1ef810e0dc37c62fedb
Kene <kayyungx@gmail.com>
2023-05-10 11:16:45 +0100

    render list of job types

--- components/home/welcome/Welcome.jsx
+++ components/home/welcome/Welcome.jsx
@@ -3,7 +3,7 @@ import {
   View,
   Text,
   TextInput,
-  Images,
+  Image,
   TouchableOpacity,
   FlatList,
 } from "react-native";
@@ -12,8 +12,13 @@ import { useRouter } from "expo-router";
 import styles from "./welcome.style";
 import { SIZES, icons } from "../../../constants";
 
+const jobTypes = ["Full Time", "Part Time", "Internship", "Freelance"];
+
 const Welcome = () => {
   const router = useRouter();
+
+  const [activeJobType, setActiveJobType] = useState("Full Time");
+
   return (
     <View>
       <View style={styles.container}>
@@ -23,8 +28,37 @@ const Welcome = () => {
 
       <View style={styles.searchContainer}>
         <View style={styles.searchWrapper}>
-          <TextInput style={styles.searchInput} />
+          <TextInput
+            style={styles.searchInput}
+            value=""
+            onChange={() => {}}
+            placeholder="What are you looking for?"
+          />
         </View>
+        <TouchableOpacity style={styles.searchBtn} onPress={() => {}}>
+          <Image source={icons.search} style={styles.searchBtnImage} />
+        </TouchableOpacity>
+      </View>
+
+      <View style={styles.tabsContainer}>
+        <FlatList
+          data={jobTypes}
+          renderItem={({ item }) => (
+            <TouchableOpacity
+              style={styles.tab(activeJobType, item)}
+              onPress={() => {
+                setActiveJobType(item);
+                router.push(`/search/${item}`);
+              }}
+            >
+              <Text style={styles.tabText(activeJobType, item)}>{item}</Text>
+            </TouchableOpacity>
+          )}
+          keyExtractor={(item) => item}
+          contentContainerStyle={{ columnGap: SIZES.small }}
+          horizontal
+          showsHorizontalScrollIndicator={false}
+        />
       </View>
     </View>
   );
