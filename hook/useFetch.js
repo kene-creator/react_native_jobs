11153f6baa7d085ad67313071c0480ea6c47733d
Kene <kayyungx@gmail.com>
2023-05-12 16:36:45 +0100

    update custome hook

--- hook/useFetch.js
+++ hook/useFetch.js
@@ -1,7 +1,10 @@
 import { useState, useEffect } from "react";
 import axios from "axios";
+import { RAPID_API_KEY } from "@env";
 
-const useFetch = ({ endpoint }) => {
+const rapidApiKey = RAPID_API_KEY;
+
+const useFetch = ({ endpoint, query }) => {
   const [data, setData] = useState([]);
   const [loading, setLoading] = useState(false);
   const [error, setError] = useState(null);
@@ -10,13 +13,11 @@ const useFetch = ({ endpoint }) => {
     method: "GET",
     url: `https://jsearch.p.rapidapi.com/${endpoint}`,
     headers: {
-      "X-RapidAPI-Key": "",
+      "X-RapidAPI-Key": rapidApiKey,
       "X-RapidAPI-Host": "jsearch.p.rapidapi.com",
     },
     params: {
-      query: "Python developer in Texas, USA",
-      page: "1",
-      num_pages: "1",
+      ...query,
     },
   };
 };
