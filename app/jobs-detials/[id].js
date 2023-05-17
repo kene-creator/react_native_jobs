a059302397269fb389bacf5bf2ca3ceb6a82b818
Kene <kayyungx@gmail.com>
2023-05-17 15:06:55 +0100

    update job footer component

--- app/jobs-detials/[id].js
+++ app/jobs-detials/[id].js
@@ -119,6 +119,12 @@ function JobDetials() {
             </View>
           )}
         </ScrollView>
+        <JobFooter
+          url={
+            data[0]?.job_google_link ??
+            "https://careers.google.com/jobs/results/"
+          }
+        />
       </>
     </SafeAreaView>
   );
